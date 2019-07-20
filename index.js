21fada6778dc365d28e9720ac4d7cf2389241dde
seun jay <seunjay92@gmail.com>
2019-07-20 08:26:13 +0100

    Connect to MongoDB database remotely

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+dotenv.config();
+
+//Connect to db
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+  console.log("Connected to DB!");
+});
+
 app.listen(5000, () => {
   console.log("Server up and running");
 });
