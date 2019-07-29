bcdf4804aa633b3934ac39a96bbe243aaf383b98
seun jay <seunjay92@gmail.com>
2019-07-29 18:25:13 +0100

    define Edit component

---
+++ client/src/components/contacts/Edit.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Edit(props) {
+  return (
+    <div>
+      <h1>Edit page</h1>
+      <Link to="/">Go Back</Link>
+    </div>
+  );
+}
