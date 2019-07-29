18832e896abce893de5903a80fb20c2b13adbfbd
seun jay <seunjay92@gmail.com>
2019-07-29 18:01:57 +0100

    import Contacts component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/layouts/NavBar";
+import Contacts from "./components/contacts/Contacts"
 import CreateContact from "./components/contacts/CreateContact"
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,7 +12,8 @@ function App() {
       <NavBar branding="Contact Manager" />
       <div className="container">
         <Switch>
-          <Route exact path="/contact/create" component={CreateContact}/>
+          <Route exact path="/" component={Contacts} />
+          <Route exact path="/contact/create" component={CreateContact} />
         </Switch>
       </div>
     </Router>
