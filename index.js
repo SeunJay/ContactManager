25908d7bb7ba0e7e9ac684d10153f422855f9131
seun jay <seunjay92@gmail.com>
2019-07-29 11:01:04 +0100

    change code for connection to database

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+//const bodyParser = require("body-parser");
 const contactsRoute = require("./routes/contact");
+const cors = require("cors");
 
 dotenv.config();
 
-//Connect to db
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-  console.log("Connected to DB!");
-});
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
+// app.use(bodyParser.json());
 
 //Middleware
 app.use(express.json());
 
+app.use(cors());
+
+//Connect to db
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+  (error, connect) => {
+    if (error) {
+      console.log("An error occured", error);
+      return;
+    }
+    console.log("Connected to DB!");
+  }
+);
+
 //routes middleware
 app.use("/contacts", contactsRoute);
 
-app.listen(5000, () => {
-  console.log("Server up and running");
+app.listen(4000, () => {
+  console.log("Server up and running on port 4000");
 });
