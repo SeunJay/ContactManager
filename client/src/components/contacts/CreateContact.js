7e3f0070cb5b0a1df082a34d5ccc197de8390e02
seun jay <seunjay92@gmail.com>
2019-07-29 10:54:46 +0100

    change nav bar color to blue

--- client/src/components/contacts/CreateContact.js
+++ client/src/components/contacts/CreateContact.js
@@ -1,25 +1,71 @@
-import React, {useState} from 'react';
-import TextInputGroup from "../layouts/TextInputGroup"
+import React, { useState, useEffect } from "react";
+import TextInputGroup from "../layouts/TextInputGroup";
+import axios from "axios";
 
 export default function CreateContact() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    phone:""
-  })
+    phone: ""
+  });
+
+  const getContacts = async () => {
+    await axios
+      .get("http://localhost:4000/contacts")
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
 
   const [errors, setErrors] = useState({});
 
-  const onChange = e =>{
-    const {name, value}= e.target;
-    setFormData({...formData, [name]: value})
-  }
+  const onChange = e => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    //check for errors
+    if (formData.name === "") {
+      setErrors({ name: "Name is required" });
+    }
+
+    if (formData.email === "") {
+      setErrors({ email: "Name is required" });
+    }
+
+    if (formData.phone === "") {
+      setErrors({ phone: "Name is required" });
+    }
+
+    const obj = {
+      name: formData.name,
+      email: formData.email,
+      phone: formData.phone
+    };
+
+    axios
+      .post("http://localhost:4000/contacts", obj)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+
+    setFormData({
+      name: "",
+      email: "",
+      phone: ""
+    });
+  };
 
   return (
     <div className="card mb-3">
       <div className="card-header center">Add Contact</div>
       <div className="card-body">
-        <form onSubmit>
+        <form onSubmit={onSubmit}>
           <TextInputGroup
             label="Name"
             name="name"
