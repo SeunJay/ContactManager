d0ccb127d4f7675c9df082e2de324ff619b9a534
seun jay <seunjay92@gmail.com>
2020-06-12 16:03:33 +0100

    lil fix

--- index.js
+++ index.js
@@ -2,18 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-//const bodyParser = require("body-parser");
+const path = require("path");
 const contactsRoute = require("./routes/contact");
 const cors = require("cors");
 
 dotenv.config();
-
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: false
-//   })
-// );
-// app.use(bodyParser.json());
 
 //Middleware
 app.use(express.json());
@@ -35,6 +28,15 @@ mongoose.connect(
 
 //routes middleware
 app.use("/contacts", contactsRoute);
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(4000, () => {
   console.log("Server up and running on port 4000");
